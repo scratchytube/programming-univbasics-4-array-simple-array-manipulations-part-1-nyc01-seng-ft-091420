0acc0e8bc70fcbf3db111a06e58e25a6d2a86502
Brian Sahota <bpsahota@gmail.com>
2020-08-31 19:34:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args
+  array.pop[2]
+end
+
+def using_shift 
+  array.shift 
+end
+
+def shift_with_args
+array.shift[2]
+end
